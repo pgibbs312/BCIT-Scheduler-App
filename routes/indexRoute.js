726de39725a223637b5c7f496f3c867b436e584e
jwarhurst22 <jawarhurst@hotmail.com>
2022-02-10 18:37:27 -0800

    landing page route & skeleton

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require("../middleware/check_auth");
 
+router.get("/", (req, res) => {
+    res.render("landing")
+})
+
 router.get("/home", ensureAuthenticated, (req, res) => {
     res.render("index", {
         user: req.user,
