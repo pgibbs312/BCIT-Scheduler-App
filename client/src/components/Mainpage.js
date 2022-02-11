366afaccb72e09c4e3259bfc1ce43be529218916
DESKTOP-NC301C1\svs02 <svs02po@gmail.com>
2022-02-11 00:30:14 -0800

    build Front page

---
+++ client/src/components/Mainpage.js
@@ -0,0 +1,22 @@
+import campus_back from '../images/campus.jpg';
+import bcit_icon from '../images/bcit-icon.png'
+import './Mainpage.css';
+
+function Mainpage() {
+    return (
+        <div className='Homepage'>
+
+            <body className='ImageText'>
+                <img src={bcit_icon} className="HomeLogoContainer" alt="Logo" />
+                <img src={campus_back} className="HomeImageContainer" alt="Background" />
+                <div className='TextBox'>
+                    <p className='Title1'>Tech Hub</p>
+                    <p className='Title2'>Study Room Booking System</p>
+                    <button className='EnterButton'>Enter</button>
+                </div>
+            </body >
+        </div >
+    );
+};
+
+export default Mainpage;
