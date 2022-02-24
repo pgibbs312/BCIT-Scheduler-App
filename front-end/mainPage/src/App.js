b69d0a376980d1184884c7d45604f5f026637309
DESKTOP-NC301C1\svs02 <svs02po@gmail.com>
2022-02-24 01:23:51 -0800

    add dummy calander img

--- front-end/mainPage/src/App.js
+++ front-end/mainPage/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar.js';
 import Header from './components/Header';
 import Fifth_floor from './components/fifth_floor';
+import Calender from './components/calander'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Header />
       <Fifth_floor />
+      <Calender />
     </>
   );
 }
---
+++ front-end/mainPage/src/components/calander.js
@@ -0,0 +1,14 @@
+import React from "react";
+import calender_img from "../calender.jpg"
+
+const calender = () => {
+    return (
+        <div className="imgWrapper">
+            <img className="fifth" src={calender_img} alt="calender"></img>
+        </div>
+
+    )
+}
+
+
+export default calender
